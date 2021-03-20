bc692e509b8935a582056520d9509d4c62aee887
Patrick Liesveld <pliesveld@users.noreply.github.com>
2021-03-20 00:44:37 -0400

    Importing project into CLion

---
+++ cmake-build-debug/CMakeFiles/treadmill.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/treadmill.dir/ultrasonicRangingTreadmill.c.o"
+  "treadmill"
+  "treadmill.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/treadmill.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
